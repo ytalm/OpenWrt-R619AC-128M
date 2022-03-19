2b848f9752464d7eeabc63fe2ab6f8e2a67bdace
ytalm <75474524+ytalm@users.noreply.github.com>
2022-03-19 23:17:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,4 +20,4 @@ sed -i 's/192.168.1.1/172.16.10.1/g' ./package/base-files/files/bin/config_gener
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认主题Modify default THEME
-sed -i 's/luci-theme-bootstrap/luci-theme-argonv/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
